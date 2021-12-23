38535af3658ac64ba1c8e07753a95d3adfc67a68
Breilys <breiliortega@gmail.com>
2021-12-23 11:15:42 -0500

    conexion con socket en main

--- src/app.js
+++ src/app.js
@@ -3,12 +3,22 @@ const cors = require('cors');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const { Server } = require('socket.io');
 
 const { createServer } = require('http');
 const { PORT, ORIGIN_URL } = require('./config');
 
 const app = express();
 const server = createServer(app);
+
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 //settings
 app.set('port', PORT);
@@ -39,5 +49,8 @@ fs.readdirSync(`${__dirname}/routes`).forEach((file) => {
   const route = require(`./routes/${file}`);
   app.use(`/api/${name}`, route);
 });
+
+
+require('./sockets')(io);
 
 module.exports = { server, app };
